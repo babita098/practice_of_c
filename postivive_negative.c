d84a1e9e2ada294763d19cb678cf1d1eecdf31b0
Babita Awasthi <babitaawasthi@Babitas-MacBook-Air.local>
2026-02-02 13:39:18 +0545

    comment missing in previous version, now comment added in code

--- postivive_negative.c
+++ postivive_negative.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main(){
-int num;
-    printf("enter an integer num");
+int num; // initailize variable
+    printf("enter an integer num"); // allowing user to enter a number
     scanf("%d" , &num);
-    if (num > 0)
-    printf(" the number is positive .\n");
-    else if (num < 0)
-    printf(" the number is negative .\n");
+    if (num > 0) // checking the number is greater than 0 or not
+    printf(" the number is positive .\n"); // if condition match printing the number is positive
+    else if (num < 0) // checking the number is less than 0 or not
+    printf(" the number is negative .\n"); // if condition match printing the number is negative
     else
-    printf(" the number is zero .\n");
+    printf(" the number is zero .\n"); // if both contidion do not match, the numbe ris 0
     return 0;
 }
--- rem.c
+++ rem.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main(){
- int num, reminder, result;
- printf("enter an integer number num");
+ int num, reminder, result; // initailizing needed variables
+ printf("enter an integer number num"); // allowing user to enter a number
  scanf("%d", &num);
- reminder = num % 2;
- printf("reminder is %d\n" , reminder);
- result = num - 5;
+ reminder = num % 2; //calculating remainder
+ printf("reminder is %d\n" , reminder); //printing remainder
+ result = num - 5; //subtracting 5 from the number by the user
  printf("result is %d", result);
 return 0;
 }
